a7ffb72994a8de32dddb97cfbfd7fe26658c6973
Sergey <Shestakowsy@gmail.com>
2023-12-25 04:29:15 +0300

    aDDED STYLE

--- simpleForm/vueComponents.js
+++ simpleForm/vueComponents.js
@@ -2,21 +2,33 @@ var compBase = {
   props: {
     'text': String,
     'req': Boolean,
-    'rules':Array
+    'valudateonload': { type:[ Boolean,String], default: false },
+    'requretext': { type: String, default: 'The value must be filled in!!!' },
+    'rules': [],
+    'inputStyle': { type:[Object,String]  },
+    'inputClass': { type: String ,default:'def-inp' } 
   },
   data: function () {
     return {
       valueInt: null,
       valid: true,
-      reqText: 'Значение должно быть заполнено!!!',
+      
       notValidText: null
     }
   },
+    computed: {
+        inputClasses() {
+            return {
+                'invalid': !this.valid,
+                [this.inputClass]: this.inputClass // добавляем переданный класс
+            };
+        }
+    },
   watch: {
     immediate: true,
     value(val) {
-      console.log(val)
-      this.valueInt = val;
+        this.valueInt = val;
+        this.Validate(val)
     }
   },
   methods: {
@@ -28,43 +40,81 @@ var compBase = {
    ,
     valChanged(event) {
       this.virtChange( event.target.value)
-      this.Validate(event.target.value)
+     
     },
 
-    Validate(val) {
-        this.valid = true
-        this.notValidText = '';
-        let errList=[]
+      Validate(val) {
+          if (typeof val === "undefined") {
+              val = this.value;
+          }
+          this.valid = true
+          let errList = []
+          this.notValidText = null;
 
       if (this.req && !val) {
         this.valid = false
-        this.notValidText = this.reqText;
+        this.notValidText = this.requretext;
       } else {
-      
-         if(this.rules){
-          this.rules.forEach(element => {
+          try {
+              if (this.rules) {
+                  this.rules.forEach(element => {
 
-             let valResult=element(val)
-             {
-                if(valResult!==true){
-                  errList.push(valResult)
-                  this.valid = false
-                }
-             }
-          });
-          if(!this.valid){
-            this.notValidText=errList.join('\n');          
+                      let valResult = element(val)
+                      {
+                          if (valResult !== true) {
+                              errList.push(valResult)
+                              this.valid = false
+                          }
+                      }
+                  });
+                  if (!this.valid) {
+                      this.notValidText = errList.join('\n');
+                  }
+              }
+             
+          }
+          catch (error)
+          {
+              console.error("Error while validation rules executing: "+error)
           }
         }
-      }
-    },
+
+        console.log(this.text+' valid = '+this.valid)
+      },
+
   },
   mounted: function () {
-    this.valueInt = this.value
+      this.valueInt = this.value
+      if (this.valudateonload==true) { 
+        this.Validate(this.value)
+      }
+
   }
 }
+Vue.component('kf-field', {
+    mixins: [compBase],
+    props: {
+        'value': { type: String },
 
-Vue.component('kf-text', {
+    },
+    methods: {
+        valChanged(event) {
+            this.$emit('input', event.target.value);
+        }
+    },
+
+    template: `
+        <input 
+            :class="inputClasses" 
+            :style="inputStyle"
+            :title="notValidText" 
+            v-bind:value="value"
+            v-on:input="valChanged($event)"
+        />
+    `
+})
+
+Vue.component('kf-input', {
   mixins: [compBase],
   props: {
     'value': { type: String },
@@ -74,19 +124,20 @@ Vue.component('kf-text', {
   },
   template: `
    <div class="flex-row" >
-   <h3 class="title-col" >{{ text }}:</h3>
+   <div class="title-col"  >{{ text }}:</div>
    
    <input 
-    class="value-col inp " :class="{ invalid: !valid }" 
+    :class="inputClasses" 
+    :style="inputStyle"
     :title="notValidText" 
      v-bind:value="value"
      v-on:input="valChanged($event)"
    />
 
-   <img v-if="!valid" src="invalid.png"></img> 
    </div>
  `
 })
+
 Vue.component('kf-date', {
   mixins: [compBase],
   props: {
@@ -100,7 +151,8 @@ Vue.component('kf-date', {
    <h3 class="title-col" >{{ text }}:</h3>
  
    <input type='date'
-    class="value-col inp " :class="{ invalid: !valid }" 
+   :class="inputClasses" 
+    :style="inputStyle"
     :title="notValidText" 
      v-bind:value="value"
      v-on:input="valChanged($event)"
@@ -109,6 +161,7 @@ Vue.component('kf-date', {
    </div>
  `
 })
+
 Vue.component('kf-combo', {
   mixins: [compBase],
   props: {
@@ -123,7 +176,9 @@ Vue.component('kf-combo', {
    <h3 class="title-col" >{{ text }}:</h3>
 
    <select 
-    class="value-col inp " :class="{ invalid: !valid }" :title="notValidText" 
+   :class="inputClasses" 
+    :title="notValidText" 
+    :style="inputStyle"
     v-model="valueInt"
     v-on:input="valChanged($event)"
   >
@@ -153,14 +208,17 @@ Vue.component('kf-num', {
    <h3 class="title-col" >{{ text }}:</h3>
    
    <input 
-    class="value-col inp " :class="{ invalid: !valid }" 
+    :class="inputClasses" 
+    :style="inputStyle" 
     :title="notValidText" 
      v-bind:value="valueInt"
      v-on:input="valChanged($event)"
      onblur="onBlur(this)"
      onfocus="onFocus(this)"     
    />
+
    <img v-if="!valid" src="invalid.png"></img> 
+
    </div>
  `
 })
@@ -174,7 +232,6 @@ Vue.component('kf-num', {
 function onBlur(control) {
   control.type = "text";
   control.value = formatNum(control.value)
-
 
 }
 
